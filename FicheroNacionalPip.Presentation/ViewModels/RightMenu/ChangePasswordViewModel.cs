1aa67831e23a0c52e00a6e8845e0b308d0eb9637
Jose <jlarapr@applicapr.com>
2025-04-24 18:33:04 -0400

    Backup: 2025.04.24

--- FicheroNacionalPip.Presentation/ViewModels/RightMenu/ChangePasswordViewModel.cs
+++ FicheroNacionalPip.Presentation/ViewModels/RightMenu/ChangePasswordViewModel.cs
@@ -2,7 +2,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace FicheroNacionalPip.Presentation.ViewModels.RightMenu;
 
-public partial class ChangePasswordViewModel: ObservableObject {
+public partial class ChangePasswordViewModel : ObservableObject {
     [ObservableProperty] private string _myTitle;
 
     public ChangePasswordViewModel() {
